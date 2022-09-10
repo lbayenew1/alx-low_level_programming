0166f6194263604b443f00ff6cbd53fe3b2c1a8e
lbayenew1 <asterbahran@gmail.com>
2022-09-10 14:22:38 -0700

    task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+/**
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ * this program prints "with proper program"
+ * Return: 0
+ */
+
 int main()
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
