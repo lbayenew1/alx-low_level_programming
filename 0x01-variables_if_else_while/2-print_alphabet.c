2f6166741280c5cfc80af1003abe0880ab23850c
lbayenew1 <asterbahran@gmail.com>
2022-09-15 01:31:22 -0700

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-    char ch;
-    
-    for(ch='a'; ch<='z'; ch++)
-    {
-        putchar(ch);
-    }
+	char ch;
 
+	for(ch='a'; ch<='z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
 	return  (0);
 }
