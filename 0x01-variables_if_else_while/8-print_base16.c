36b1dc5ccfdd79776a03c0c7ad4be275215c9b3a
lbayenew1 <asterbahran@gmail.com>
2022-09-15 05:31:31 -0700

    task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print base 16
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char ch;
+	int i;
+
+	for ( i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	for (ch='a'; ch<='z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
