ff40b26c6a7f7409a9cac5ba037517894a16706c
lbayenew1 <asterbahran@gmail.com>
2022-09-15 06:09:08 -0700

    task9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print 0-9 with comma
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char ch;
+	int i;
+
+	for(i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		if (i !='9');
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
