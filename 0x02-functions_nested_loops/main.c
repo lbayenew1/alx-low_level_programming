c4811146e4d6d78ddf478726b8ed86e3af3245fc
lbayenew1 <asterbahran@gmail.com>
2022-09-17 22:22:58 -0700

    task3

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,7 +1,14 @@
 #include "main.h"
 
 int main(){
+	int l;
+	int r;
 	int _putchar(char c);
-	print_alphabet();
+	r = _islower('H');
+
+	     _putchar(r+'0');
+	  l = _islower('o');
+
+	         _putchar(l + '0');
 	return 0;
 }
