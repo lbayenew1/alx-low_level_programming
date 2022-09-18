3534b893ac79face9f570808e3676ca85d771600
lbayenew1 <asterbahran@gmail.com>
2022-09-18 00:22:42 -0700

    task6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - all are positive number
+ * @n: character
+ * Return: positive number
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n < 0)
+	{
+		n = -(n)
+		return (n);
+	}
+}
