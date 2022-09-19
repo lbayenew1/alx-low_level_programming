1748086d3718469fafeebad1163b604eaf032376
lbayenew1 <asterbahran@gmail.com>
2022-09-19 12:37:54 -0700

    task6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+#include "main.h"
+
+
+
+/**
+ * puts2 - prints every two character
+ * @str: string
+ * Return: nothing
+ */
+
+
+
+void puts2(char *str)
+
+{
+
+	int i = 0;
+
+
+
+	while (str[i] != '\0')
+
+	{
+
+		if (i % 2 == 0)
+
+			_putchar(str[i]);
+
+		i++;
+
+	}
+
+	_putchar(‘\n’);
+
+}
+
