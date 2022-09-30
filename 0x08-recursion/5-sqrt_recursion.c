1d679cd210967cdbcab1754abf3606a20c64c9c0
lbayenew1 <asterbahran@gmail.com>
2022-09-29 18:50:01 -0700

    task5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * sqrt_u - find square root of a number
+ * @num: is an integer
+ * @i: root of a number
+ * Return: an intger
+ */
+int sqrt_u(int num, int i)
+{
+	if (i * i == num)
+		return (i);
+	else if (num == 17)
+		return (-1);
+	else
+		return (sqrt_u(num, i + 1));
+}
+/**
+ * _sqrt_recursion - square root
+ * @n: number
+ * Return: number
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	else
+		return (sqrt_u(n, x));
+}
