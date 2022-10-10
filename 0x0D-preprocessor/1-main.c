325a58684ca21c09882bbcebd7eff7bacd5630d2
lbayenew1 <asterbahran@gmail.com>
2022-10-10 10:03:26 -0700

    task1

--- 0x0D-preprocessor/1-main.c
+++ 0x0D-preprocessor/1-main.c
@@ -1,6 +1,6 @@
-#include "0-object_like_macro.h"
+#include "1-pi.h"
 
-#include "0-object_like_macro.h"
+#include "1-pi.h"
 
 #include <stdio.h>
 
@@ -20,13 +20,17 @@ int main(void)
 
 {
 
-    int s;
+    float a;
+
+    float r;
 
 
 
-    s = 98 + SIZE;
+    r = 98;
 
-    printf("%d\n", s);
+    a = PI * r * r;
+
+    printf("%.3f\n", a);
 
     return (0);
 
