f7d293058c0348ca5a052df4c7e3ec1953422b9c
lbayenew1 <asterbahran@gmail.com>
2022-10-10 10:35:57 -0700

    task2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - excute fil name
+ * Return: 0
+ */
+
+int main(void)
 {
 	char s[] = __FILE__;
 	int i;
 
-	for (i=0; s[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 		putchar(s[i]);
 	putchar('\n');
 	return (0);
