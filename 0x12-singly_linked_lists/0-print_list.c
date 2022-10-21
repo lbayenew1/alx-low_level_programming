20f31b107feba1262fdbdb2767549e9e30dffae2
lbayenew1 <asterbahran@gmail.com>
2022-10-21 09:58:19 -0700

    task0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "lists.h"
 
 /**
  * print_list - print number of node
@@ -12,7 +12,7 @@ size_t print_list(const list_t *h)
 	node = 0;
 	while(h)
 	{
-		if (h -> str = '\0')
+		if (h -> str == NULL)
 			printf("[0] (nil)");
 		else
 			printf("[%d] %s\n", h -> len, h -> str);
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 #include <stdlib.h>
+#include <stdio.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -20,3 +21,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 int _putchar(char c);
+#endif
