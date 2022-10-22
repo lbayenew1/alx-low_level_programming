3ac6413374123e670dc632d86287dbd4173f4bb9
lbayenew1 <asterbahran@gmail.com>
2022-10-22 05:20:14 -0700

    task4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,6 +1,18 @@
 #include "lists.h"
 
+/**
+ * free_list - free all allocated
+ * @head: pointer to linked list
+ */
 void free_list(list_t *head)
 {
-	free(head);
+	list_t *tmp;
+	tmp = head;
+	while (tmp)
+	{
+		free(tmp->str);
+		free(tmp);
+		tmp = tmp->next;
+	}
+	free(tmp);
 }
