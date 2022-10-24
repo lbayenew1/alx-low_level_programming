f6cf20c2dbee827b610b0c2567344bbe784a8778
lbayenew1 <asterbahran@gmail.com>
2022-10-24 09:49:50 -0700

    task1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_t - it print number of node
+ * @h: pointer of the first node
+ * Return: number of node
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count, i;
+
+	count = 0;
+	for (i = 0; h != NULL; i++)
+	{
+		count = count + 1;
+		h = h->next;
+	}
+	return (count);
+}
