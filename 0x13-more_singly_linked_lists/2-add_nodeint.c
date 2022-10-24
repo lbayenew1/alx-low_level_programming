a4cebf3daf064ac73ac28d6531aabe1e80c05082
lbayenew1 <asterbahran@gmail.com>
2022-10-24 13:33:18 -0700

    tas2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -11,6 +11,8 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	listint_t *newnode;
 
 	newnode = malloc(sizeof(listint_t));
+	if (newnode == NULL)
+		return (NULL);
 	if (*head == NULL)
 	{
 		*head = newnode;
