4b5c47974e18006da99ee01b1d51051717ef1e2b
lbayenew1 <asterbahran@gmail.com>
2022-10-25 11:51:12 -0700

    task9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,7 +18,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	tmp = *head;
 	if (idx == 0)
 	{
-		newnode->next = temp;
+		newnode->next = tmp;
 		*head = newnode;
 		return (newnode);
 	}
