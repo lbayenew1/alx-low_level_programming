bc2e7234721b712a65ffbaa871efa2bb6a3bb5d1
lbayenew1 <asterbahran@gmail.com>
2022-10-25 10:20:44 -0700

    task7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -17,6 +17,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	for (i = 0; i != index; i++)
 	{
 		tmp = tmp->next;
+		if (tmp == NULL)
+			return (NULL);
 	}
 	return (tmp);
 }
