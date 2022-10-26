ff325566064f57f02d0d8173adde91c913ab7a0a
lbayenew1 <asterbahran@gmail.com>
2022-10-26 12:58:55 -0700

    task10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -19,7 +19,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			prevnode = prevnode->next;
 		}
 	}
-	if (*head == NULL)
+	if (*head == NULL || (prevnode->next == NULL && index != 0))
 		return (-1);
 	tmp = prevnode->next;
 	if (index != 0)
