443e59599c9fc952c111d6f2bdabd02d647b343e
lbayenew1 <asterbahran@gmail.com>
2022-10-30 13:31:49 -0700

    task4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * clear_bit - set 1 at given index
+ * @n: unsigned long int
+ * @index: unsigned int
+ * Return: 1 for success 0r -1 unsucess
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int i;
+
+	if (index > 63)
+		return (-1);
+	if (*n == 0)
+	{
+		*n = 0;
+		return (1);
+	}
+	i = 1 << index;
+	*n = *n - i;
+	return (1);
+}
