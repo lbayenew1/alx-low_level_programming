5bceeedeaa37ac5b4e247dfd8a0192d819ccce73
lbayenew1 <asterbahran@gmail.com>
2022-10-30 10:12:32 -0700

    task1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_binary - from num to binary
+ * @n: unsigned long int
+ * Return: nothing
+ */
+void print_binary(unsigned long int n)
+{
+	if (n >> 0)
+	{
+		if (n >> 1)
+			print_binary(n >> 1);
+		putchar((n&1) + '0');
+	}
+	else
+		putchar('0');
+}
