57352a0b5b6a39d3cab4fe439c927a369d751bd3
lbayenew1 <asterbahran@gmail.com>
2022-10-30 13:43:58 -0700

    task4

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -17,11 +17,6 @@ int clear_bit(unsigned long int *n, unsigned int index)
 		i = 1 << index;
 		*n = *n - i;
 	}
-	else if (*n < 0)
-	{
-		i = 1 << index;
-		*n = *n + i;
-	}
 	else
 		*n = 0;
 	return (1);
